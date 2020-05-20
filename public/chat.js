6d0648ae3274109a7c3bc9075032abd7e30c5a93
Zprogrammer-code <skatboyza124@hotmail.com>
2020-05-20 12:45:36 -0600

    gh -pages could use some file orientation notes on whagt gets displayed

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 // make connection
 
-const socket = io.connect("https://zprogrammer-code.github.io/ChatRoomApp/src/public/")
+const socket = io.connect("https://zprogrammer-code.github.io/ChatRoomApp/public/")
 
 // query DOM
 
